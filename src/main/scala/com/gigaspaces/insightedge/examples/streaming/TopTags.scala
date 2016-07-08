d8a74586917c8443e12860e4df623620494d901c
fe2s <aka.fe2s@gmail.com>
2016-07-08 05:58:03 -0400

    bucketing

--- src/main/scala/com/gigaspaces/insightedge/examples/streaming/TopTags.scala
+++ src/main/scala/com/gigaspaces/insightedge/examples/streaming/TopTags.scala
@@ -1,15 +1,12 @@
 package com.gigaspaces.insightedge.examples.streaming
 
 import com.gigaspaces.scala.annotation._
-import com.gigaspaces.spark.model.GridModel
-
 import scala.beans.BeanProperty
 
 /**
   * @author Oleksiy_Dyagilev
   */
 case class TopTags(
-
                     @SpaceId(autoGenerate = true)
                     @BeanProperty
                     var id: String,
@@ -19,8 +16,7 @@ case class TopTags(
 
                     @BeanProperty
                     var batchTime: Long
-
-                  ) extends GridModel {
+                  ) {
 
   def this(tagsCount: java.util.Map[Int, String]) = this(null, tagsCount, System.currentTimeMillis)
 
