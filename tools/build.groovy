8b7acda8060d1df106432d6611884ef929ac97d3
Danylo Hurin <danylo@gigaspaces.com>
2016-07-14 05:09:16 +0300

    IE-100: add opensapce repo

--- tools/build.groovy
+++ tools/build.groovy
@@ -1,9 +1,3 @@
+sh "sbt clean test assembly -no-colors"
+
 sh "mvn clean install"
-
-
-try {
-    sh "sbt clean test assembly -no-colors"
-} finally {
-    sh "sbt clean test assembly -no-colors"
-}
-
