26b6884e9f86429b16a4c3853f181ce8d5638d25
Yohana Khoury <yohana@yohanakhoury.com>
2017-06-27 17:34:46 +0300

    IE-208 added examples: LoadDataset and PersistDataset

---
+++ src/main/scala/org/insightedge/examples/basic/LoadDataset.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016, GigaSpaces Technologies, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.insightedge.examples.basic
+
+import org.apache.spark.sql.SparkSession
+import org.insightedge.spark.context.InsightEdgeConfig
+import org.insightedge.spark.implicits.all._
+
+/**
+  * Loads Products from Data Grid as DataFrame and runs filtering.
+  */
+object LoadDataset {
+
+  def main(args: Array[String]): Unit = {
+    val settings = if (args.length > 0) args else Array("spark://127.0.0.1:7077", "insightedge-space", "insightedge", "127.0.0.1:4174")
+    if (settings.length < 4) {
+      System.err.println("Usage: LoadDataset <spark master url> <space name> <space groups> <space locator>")
+      System.exit(1)
+    }
+    val Array(master, space, groups, locators) = settings
+    val config = InsightEdgeConfig(space, Some(groups), Some(locators))
+    val spark = SparkSession.builder
+      .appName("example-load-dataset")
+      .master(master)
+      .insightEdgeConfig(config)
+      .getOrCreate()
+
+    import spark.implicits._
+    val ds = spark.read.grid[Product].as[Product]
+    ds.printSchema()
+    val count = ds.filter( o => o.quantity < 5).count()
+    println(s"Number of products with quantity < 5: $count")
+    spark.stopInsightEdgeContext()
+  }
+
+}
